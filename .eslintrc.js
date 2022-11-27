4d60b118a6127279041d385a0cabf5baea4cc2d7
Ycow <skypnal12@gmail.com>
2022-11-27 21:39:43 +0900

    docs : eslint plugin add

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [
       'error',
       {
