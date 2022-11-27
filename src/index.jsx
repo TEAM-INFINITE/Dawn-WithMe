afc2c909ba097414ab6b50510973bc43bb5917cb
Ycow <skypnal12@gmail.com>
2022-11-28 01:31:39 +0900

    chore : 폴더 구조 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyled from './GlobalStyled/Globalstyled';
+import GlobalStyled from './styles/Globalstyled';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
