07d57e038bcee124dc30b0cbb9d14197f4047279
Ycow <skypnal12@gmail.com>
2022-12-05 17:55:44 +0900

    Chore : recoil 라이브러리 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
 import GlobalStyled from './styles/Globalstyled';
 import App from './App';
 import theme from './styles/theme';
@@ -9,8 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyled />
-      <App />
+      <RecoilRoot>
+        <GlobalStyled />
+        <App />
+      </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
 );
