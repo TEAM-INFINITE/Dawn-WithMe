83d4aba219d0d6ee218deeac8c8c6d7562f103b8
Ycow <skypnal12@gmail.com>
2022-12-15 17:09:58 +0900

    Feat : 재사용 Wrapper 생성

---
+++ src/components/atoms/Wrapper/DefaultWrapper.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const PageDefaultWrapper = styled.section`
+  max-width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export default PageDefaultWrapper;
