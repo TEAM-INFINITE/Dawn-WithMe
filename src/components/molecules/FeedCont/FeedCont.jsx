6e82035298dbf8f7f23fbf0a41c413386157771a
Jeongeum <wjddma1202@naver.com>
2022-12-15 10:43:47 +0900

    Feat : 자유게시판p+img 로 구성된 content 컴포넌트 생성

---
+++ src/components/molecules/FeedCont/FeedCont.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import Img from '../../atoms/Img/Img';
+import FeedContWrapper from './styled';
+
+const FeedCont = ({ children, src }) => {
+  return (
+    <FeedContWrapper>
+      <Link to='/boardDetail'>
+        <p>{children}</p>
+        <Img src={src} alt='게시판 사진' width='100%' className='feedImg' />
+      </Link>
+    </FeedContWrapper>
+  );
+};
+
+export default FeedCont;
