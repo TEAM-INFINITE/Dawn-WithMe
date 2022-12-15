49680bab36724eb7a3db00082c983a587543235b
Jeongeum <wjddma1202@naver.com>
2022-12-15 10:52:19 +0900

    Feat : 자유게시판 레이아웃&css 구현 완료 #10

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import FreeBoard from './pages/FreeBoard/FreeBoard';
+import FeedDetail from './pages/FeedDetail/FeedDetail';
+import MyProfile from './pages/MyProfile/MyProfile';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +16,9 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/freeboard' element={<FreeBoard />} />
+          <Route path='/myprofile' element={<MyProfile />} />
+          <Route path='/FeedDetail' element={<FeedDetail />} />
         </Routes>
         <ReactQueryDevtools />
       </BrowserRouter>
