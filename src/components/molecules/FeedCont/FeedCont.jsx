f7494cd1e44d9536a859eebf1f0369f5f293041d
Jeongeum <wjddma1202@naver.com>
2022-12-16 19:52:39 +0900

    Rename : 자유게시판 파일 및 변수 명 수정

--- src/components/molecules/FeedCont/FeedCont.jsx
+++ src/components/molecules/FeedCont/FeedCont.jsx
@@ -5,7 +5,7 @@ import FeedContWrapper from './styled';
 const FeedCont = ({ children, src }) => {
   return (
     <FeedContWrapper>
-      <Link to='/feedDetail'>
+      <Link to='/feeddetail'>
         <p>{children}</p>
         <Img src={src} alt='게시판 사진' width='100%' className='feedImg' />
       </Link>
