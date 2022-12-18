d9f018f6eb55007003a29850fa163d4b4df6ae6a
jejeday <jhjae1@naver.com>
2022-12-19 06:41:49 +0900

    Feat: 스플래쉬 템플릿 생성

---
+++ src/components/template/SplashTemplate/SplashTemplate.jsx
@@ -0,0 +1,12 @@
+import SplashView from '../../organisms/SplashView/SplashView';
+import SplashWrapper from './styled';
+
+const SplashTemplate = () => {
+  return (
+    <SplashWrapper>
+      <SplashView />
+    </SplashWrapper>
+  );
+};
+
+export default SplashTemplate;
---
+++ src/components/template/SplashTemplate/styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const SplashWrapper = styled.section`
+  margin: 0 auto;
+  max-width: 600px;
+  height: 100vh;
+  background-color: rgb(56, 28, 121);
+`;
+
+export default SplashWrapper;
