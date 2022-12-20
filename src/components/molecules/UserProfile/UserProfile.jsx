5ca50654a1b45c94339823ad11a1b8b6fb57c00a
Jeongeum <wjddma1202@naver.com>
2022-12-20 10:50:43 +0900

    Design : p태그 → 공용컴포넌트로 수정

--- src/components/molecules/UserProfile/UserProfile.jsx
+++ src/components/molecules/UserProfile/UserProfile.jsx
@@ -1,13 +1,14 @@
 import Img from '../../atoms/Img/Img';
-import { UserName, UserId, UserProfileWrapper } from './styled';
+import FeedText from '../../atoms/P/Feed/FeedText';
+import { UserProfileWrapper } from './styled';
 
 const UserProfile = ({ userName, userId, src }) => {
   return (
     <UserProfileWrapper>
       <Img src={src} alt='프로필 사진' width='42px' />
       <div className='profile-text'>
-        <UserName>{userName}</UserName>
-        <UserId>@{userId}</UserId>
+        <FeedText type='username'>{userName}</FeedText>
+        <FeedText type='userid'>@{userId}</FeedText>
       </div>
     </UserProfileWrapper>
   );
