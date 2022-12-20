3e7190d4aafa24577529ba11ac82779aa8482330
jejeday <jhjae1@naver.com>
2022-12-20 14:06:35 +0900

    Feat: 스플래쉬 로그인 버튼 카드 컴포넌트 수정 및 스타일 추가

--- src/components/molecules/ButtonLoginCard/ButtonLoginCard.jsx
+++ src/components/molecules/ButtonLoginCard/ButtonLoginCard.jsx
@@ -1,16 +1,23 @@
 import Button from '../../atoms/Button/Button';
 import ButtonLoginCardWrapper from './styled';
+import Img from '../../atoms/Img/Img';
+import iconKakaotalk from '../../../assets/images/icon-kakaotalk.png';
+import iconGoogle from '../../../assets/images/icon-google.png';
+import iconFacebook from '../../../assets/images/icon-facebook.png';
 
 const ButtonLoginCard = () => {
   return (
     <ButtonLoginCardWrapper>
       <Button width='100%' size='large' type='submit'>
+        <Img src={iconKakaotalk} width='18px' alt='카카오톡' />
         카카오톡 계정으로 로그인
       </Button>
       <Button width='100%' size='large' type='submit'>
+        <Img src={iconGoogle} width='18px' alt='구글' />
         구글 계정으로 로그인
       </Button>
       <Button width='100%' size='large' type='submit'>
+        <Img src={iconFacebook} width='11px' alt='페이스북' />
         페이스북 계정으로 로그인
       </Button>
     </ButtonLoginCardWrapper>
---
+++ src/components/molecules/ButtonLoginCard/styled.jsx
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components';
+
+const ButtonLoginCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  button + button {
+    margin-top: 10px;
+  }
+
+  img {
+    float: left;
+  }
+
+  button {
+    font-weight: 400;
+    text-align: center;
+    ${({ theme }) => {
+      return css`
+        background-color: ${theme.colors.colorBg};
+      `;
+    }}
+    &:nth-child(1) {
+      border: 1px solid #f2c94c;
+      padding-left: 17px;
+    }
+    &:nth-child(2) {
+      border: 1px solid #767676;
+      padding-left: 17px;
+    }
+    &:nth-child(3) {
+      border: 1px solid #2d9cdb;
+      padding-left: 20px;
+    }
+  }
+`;
+
+export default ButtonLoginCardWrapper;
