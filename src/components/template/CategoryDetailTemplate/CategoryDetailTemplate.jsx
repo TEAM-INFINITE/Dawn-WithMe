fd8a016a6df73f270d88b461044cc6dcac94df29
Ycow <skypnal12@gmail.com>
2022-12-20 13:30:43 +0900

    Feat : Detail Template 파일 추가

---
+++ src/components/template/CategoryDetailTemplate/CategoryDetailTemplate.jsx
@@ -0,0 +1,25 @@
+import HeaderWrapper from '../../atoms/Wrapper/HeaderWrapper';
+import MainWrapper from '../../atoms/Wrapper/MainWrapper';
+import TopNavBar from '../../molecules/TopNavBar/TopNavBar';
+import CategoryDetailFeed from '../../organisms/CategoryDetailFeed/CategoryDetailFeed';
+import TabMenu from '../../organisms/TabMenu/TabMenu';
+import CategoryDetailTemplateWrapper from './styled';
+
+const CategoryDetailTemplate = ({ detailData }) => {
+  console.log(detailData);
+  return (
+    <>
+      <HeaderWrapper>
+        <TopNavBar cont='back' more />
+      </HeaderWrapper>
+      <MainWrapper>
+        <CategoryDetailTemplateWrapper>
+          <CategoryDetailFeed data={detailData} />
+        </CategoryDetailTemplateWrapper>
+        <TabMenu />
+      </MainWrapper>
+    </>
+  );
+};
+
+export default CategoryDetailTemplate;
