93ff17558e515918294a378047c2cf2328c83ce0
STRONGSHARK <woghfghf@naver.com>
2022-12-21 10:31:32 +0900

    Feat : 프로필설정 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FreeBoardPage from './pages/FreeBoardPage/FreeBoardPage';
 import CategoryPage from './pages/CategoryPage/CategoryPage';
 import CategoryDetailPage from './pages/CategoryDetailPage/CategoryDetailPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import ProfileSettingPage from './pages/ProfileSettingPage/ProfileSettingPage';
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => {
           <Route path='/category/:name' element={<CategoryPage />} />
           <Route path='/category/:name/:id' element={<CategoryDetailPage />} />
           <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/profilesetting' element={<ProfileSettingPage />} />
         </Routes>
         <ReactQueryDevtools />
       </BrowserRouter>
