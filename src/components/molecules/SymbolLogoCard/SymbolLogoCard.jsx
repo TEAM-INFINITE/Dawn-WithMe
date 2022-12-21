7eca062c8937207b812085d848d593a72356545f
jejeday <jhjae1@naver.com>
2022-12-21 17:47:41 +0900

    Style : 스플래쉬 부엉이 로고 카드 불필요한 코드 삭제

--- src/components/molecules/SymbolLogoCard/SymbolLogoCard.jsx
+++ src/components/molecules/SymbolLogoCard/SymbolLogoCard.jsx
@@ -1,16 +1,6 @@
 import Img from '../../atoms/Img/Img';
 import SymbolLogoCardWrapper from './styled';
-// import grayFullLogo from '../../../assets/images/gray-full-logo.png';
 import titleLogo from '../../../assets/images/title-logo.png';
-
-// const SymbolLogoCard = () => {
-//   return (
-//     <SymbolLogoCardWrapper>
-//       <Img className='grayowl' src={grayFullLogo} width='200px' alt='로딩중' />
-//       <Img className='logo' src={titleLogo} width='140px' alt='타이틀 로고' />
-//     </SymbolLogoCardWrapper>
-//   );
-// };
 
 const SymbolLogoCard = ({ src }) => {
   return (
