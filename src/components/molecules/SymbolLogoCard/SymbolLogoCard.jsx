8b1503ce6f5b43816b24dd584c29e4e3e8d6f1bf
jejeday <jhjae1@naver.com>
2022-12-21 14:24:17 +0900

    Design : 스플래쉬 부엉이 크기 변경 및 중앙 배치

--- src/components/molecules/SymbolLogoCard/SymbolLogoCard.jsx
+++ src/components/molecules/SymbolLogoCard/SymbolLogoCard.jsx
@@ -1,13 +1,22 @@
 import Img from '../../atoms/Img/Img';
 import SymbolLogoCardWrapper from './styled';
-import grayFullLogo from '../../../assets/images/gray-full-logo.png';
+// import grayFullLogo from '../../../assets/images/gray-full-logo.png';
 import titleLogo from '../../../assets/images/title-logo.png';
 
-const SymbolLogoCard = () => {
+// const SymbolLogoCard = () => {
+//   return (
+//     <SymbolLogoCardWrapper>
+//       <Img className='grayowl' src={grayFullLogo} width='200px' alt='로딩중' />
+//       <Img className='logo' src={titleLogo} width='140px' alt='타이틀 로고' />
+//     </SymbolLogoCardWrapper>
+//   );
+// };
+
+const SymbolLogoCard = ({ src }) => {
   return (
     <SymbolLogoCardWrapper>
-      <Img className='grayowl' src={grayFullLogo} width='200px' alt='로딩중' />
-      <Img className='logo' src={titleLogo} width='200px' alt='타이틀 로고' />
+      <Img className='owl' src={src} width='200px' alt='캐릭터' />
+      <Img className='logo' src={titleLogo} width='140px' alt='타이틀 로고' />
     </SymbolLogoCardWrapper>
   );
 };
