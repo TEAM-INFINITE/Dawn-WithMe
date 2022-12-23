2fc53d9a2e6a2b19296c343f9989f2e9ffcfa344
Ycow <skypnal12@gmail.com>
2022-12-23 20:26:21 +0900

    Style : postButton animation 수정

--- src/components/molecules/PostMenu/styled.jsx
+++ src/components/molecules/PostMenu/styled.jsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const PostMenuWrapper = styled.div`
   width: 50px;
