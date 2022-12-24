4e3d9deb391c0c96d66f6e94e5d307bdc95ec968
Ycow <skypnal12@gmail.com>
2022-12-24 14:37:48 +0900

    Feat : 카테고리 게시글 추가 페이지,템플릿 추가

---
+++ src/components/template/CategoryPostTemplate/styled.jsx
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+const CategoryPostTemplateWrapper = styled.section`
+  max-width: 600px;
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+  padding: 30px 34px 0;
+
+  ${({ theme }) => {
+    return css`
+      background-color: ${theme.colors.colorBg};
+    `;
+  }}
+`;
+export default CategoryPostTemplateWrapper;
