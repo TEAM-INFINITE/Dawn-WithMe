4d1f7c0a34e3de2e5cfe911147a92b31dfe0730e
Ycow <skypnal12@gmail.com>
2022-12-26 11:12:15 +0900

    Feat : useQuery 로직 추가

--- src/pages/ProfilePage/MyProfilePage.jsx
+++ src/pages/ProfilePage/MyProfilePage.jsx
@@ -1,7 +1,16 @@
+import { useQuery } from 'react-query';
+import getMyProfile from '../../api/profile/getMyProfile';
 import MyProfileTemplate from '../../components/template/MyProfileTemplate/MyProfileTemplate';
 
 const MyProfilePage = () => {
-  return <MyProfileTemplate />;
+  const { data, isLoading, isError } = useQuery('myprofile', getMyProfile);
+
+  if (isLoading) return <p>로딩 중</p>;
+  if (isError) return <p>에러 발생</p>;
+
+  console.log(data);
+
+  return <MyProfileTemplate data={data} />;
 };
 
 export default MyProfilePage;
