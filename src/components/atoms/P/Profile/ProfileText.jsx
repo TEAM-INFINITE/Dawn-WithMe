e74918b5a5d58b5b1820c016d09dd55fe465ff45
Ycow <skypnal12@gmail.com>
2022-12-26 11:10:46 +0900

    Feat : profile Text atoms 추가

--- src/components/atoms/P/Profile/ProfileText.jsx
+++ src/components/atoms/P/Profile/ProfileText.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const ProfileText = styled.p`
-  color: ${({ fontColor }) => fontColor};
+  color: ${({ fontColor }) => fontColor || 'white'};
   font-size: ${({ fontSize }) => fontSize || '18px'};
   font-weight: ${({ fontWeight }) => fontWeight || 400};
   line-height: ${({ lineHeight }) => lineHeight || '23px'};
