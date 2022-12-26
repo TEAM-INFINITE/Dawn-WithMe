2b88e580a9cd02e0d78b915f5568f2e6895aa135
STRONGSHARK <woghfghf@naver.com>
2022-12-27 03:41:54 +0900

    Feat : 프로필설정 폼 구현

--- src/components/organisms/ProfileSettingForm/ProfileSettingForm.jsx
+++ src/components/organisms/ProfileSettingForm/ProfileSettingForm.jsx
@@ -3,40 +3,49 @@ import Button from '../../atoms/Button/Button';
 import ProfileSettingFormWrapper from './styled';
 
 const ProfileSettingForm = ({
+  inputValue,
   onChangeInputHandler,
   onSubmitButtonHandler,
-  profileValue,
-  error,
+  userError,
+  AccountError,
 }) => {
-  const { username, accountname, intro } = profileValue;
+  const { username, accountname, intro } = inputValue;
+
   return (
     <ProfileSettingFormWrapper onSubmit={onSubmitButtonHandler}>
       <TextFiledCard
+        value={username}
         type='text'
-        id='username'
-        name='email'
+        id='userName'
+        name='username'
         onChange={onChangeInputHandler}
         placeholder='2~10자 이내여야 합니다.'
+        required
         autoFocus={true}
       >
         사용자 이름
       </TextFiledCard>
+      {userError && <p>{userError}</p>}
       <TextFiledCard
+        value={accountname}
         type='text'
         id='userId'
-        name='userId'
+        name='accountname'
         onChange={onChangeInputHandler}
+        required
         placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
       >
         계정 ID
       </TextFiledCard>
-      {error && <p>{error}</p>}
+      {AccountError && <p>{AccountError}</p>}
       <TextFiledCard
+        value={intro}
         type='text'
         id='userIntro'
-        name='userIntro'
+        name='intro'
         onChange={onChangeInputHandler}
         placeholder='자신을 소개해주세요!'
+        required
       >
         소개
       </TextFiledCard>
