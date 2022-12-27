232f8b2c9b500f3e53d5ef9db30bfcc229fde377
Ycow <skypnal12@gmail.com>
2022-12-27 16:59:40 +0900

    Style : 콘솔로그 제거

--- src/api/feed/getFollowFeed.js
+++ src/api/feed/getFollowFeed.js
@@ -2,7 +2,6 @@ import { accessInstance } from '../axios-api';
 
 const getFollowFeed = async () => {
   const response = await accessInstance.get(`/post/feed/?limit=1000`);
-  console.log(response.data);
 
   return response.data;
 };
