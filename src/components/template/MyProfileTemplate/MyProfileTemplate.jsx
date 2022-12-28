edbccd1f645def3276be52b54e03d8c9774d148c
Ycow <skypnal12@gmail.com>
2022-12-28 18:10:56 +0900

    Feat : 일반 태그로 들어가 있던 부분 컴포넌트로 변경

--- src/components/template/MyProfileTemplate/MyProfileTemplate.jsx
+++ src/components/template/MyProfileTemplate/MyProfileTemplate.jsx
@@ -1,21 +1,13 @@
-import { Link } from 'react-router-dom';
 import HeaderWrapper from '../../atoms/Wrapper/HeaderWrapper';
 import MainWrapper from '../../atoms/Wrapper/MainWrapper';
 import TopNavBar from '../../molecules/TopNavBar/TopNavBar';
-import FeedCard from '../../organisms/FeedCard/FeedCard';
 import ProfileCategoryPostCard from '../../organisms/ProfileCategoryPostCard/ProfileCategoryPostCard';
 import ProfileInfoCard from '../../organisms/ProfileInfoCard/ProfileInfoCard';
 import TabMenu from '../../organisms/TabMenu/TabMenu';
-import MyProfileTemplateWrapper, {
-  MyProfileAlbumFeedWrapper,
-  MyProfileFeedShowSelectWrapper,
-  MyProfileListFeedWrapper,
-} from './styled';
-import Img from '../../atoms/Img/Img';
-import postListIcon from '../../../assets/images/icon-post-list-off.png';
-import postListOnIcon from '../../../assets/images/icon-post-list-on.png';
-import postAlbumIcon from '../../../assets/images/icon-post-album-off.png';
-import postAlbumOnIcon from '../../../assets/images/icon-post-album-on.png';
+import MyProfileTemplateWrapper from './styled';
+import ProfileFeedShowSelectBox from '../../molecules/ProfileFeedShowSelectBox/ProfileFeedShowSelectBox';
+import ProfileFeedListCard from '../../organisms/ProfileFeedListCard/ProfileFeedListCard';
+import ProfileFeedAlbumCard from '../../organisms/ProfileFeedAlbumCard/ProfileFeedAlbumCard';
 
 const MyProfileTemplate = ({
   profileData,
@@ -23,7 +15,7 @@ const MyProfileTemplate = ({
   selectCategoryData,
   onChangeSelectBoxHandler,
   onClickShowTypeChange,
-  PostShowType,
+  postShowType,
 }) => {
   return (
     <>
@@ -38,49 +30,15 @@ const MyProfileTemplate = ({
             onChange={onChangeSelectBoxHandler}
             selectCategoryData={selectCategoryData}
           />
-          <MyProfileFeedShowSelectWrapper>
-            <button
-              type='button'
-              onClick={() => {
-                onClickShowTypeChange('list');
-              }}
-            >
-              <Img
-                src={PostShowType === 'list' ? postListOnIcon : postListIcon}
-              />
-            </button>
-            <button
-              type='button'
-              onClick={() => {
-                onClickShowTypeChange('album');
-              }}
-            >
-              <Img
-                src={PostShowType === 'album' ? postAlbumOnIcon : postAlbumIcon}
-              />
-            </button>
-          </MyProfileFeedShowSelectWrapper>
-          {PostShowType === 'list' && (
-            <MyProfileListFeedWrapper>
-              {postData.map((item) => (
-                <FeedCard key={item.id} data={item} />
-              ))}
-            </MyProfileListFeedWrapper>
+          <ProfileFeedShowSelectBox
+            onClickShowTypeChange={onClickShowTypeChange}
+            postShowType={postShowType}
+          />
+          {postShowType === 'list' && (
+            <ProfileFeedListCard postData={postData} />
           )}
-          {PostShowType === 'album' && (
-            <MyProfileAlbumFeedWrapper>
-              {postData.map((item) => (
-                <li key={item.id}>
-                  <Link to={`/feeddetail/${item.id}`}>
-                    {item.image.split(', ').length > 1 ? (
-                      <Img src={item.image.split(', ')[0]} />
-                    ) : (
-                      <Img src={item.image} />
-                    )}
-                  </Link>
-                </li>
-              ))}
-            </MyProfileAlbumFeedWrapper>
+          {postShowType === 'album' && (
+            <ProfileFeedAlbumCard postData={postData} />
           )}
         </MyProfileTemplateWrapper>
         <TabMenu />
