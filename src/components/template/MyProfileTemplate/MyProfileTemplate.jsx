578c011dbeae9e7743d2b7120fa7f6af6c1bf8e4
Ycow <skypnal12@gmail.com>
2022-12-28 09:23:17 +0900

    Feat : 데이터 뿌려주기 및 구성 완료

--- src/components/template/MyProfileTemplate/MyProfileTemplate.jsx
+++ src/components/template/MyProfileTemplate/MyProfileTemplate.jsx
@@ -1,16 +1,29 @@
+import { Link } from 'react-router-dom';
 import HeaderWrapper from '../../atoms/Wrapper/HeaderWrapper';
 import MainWrapper from '../../atoms/Wrapper/MainWrapper';
 import TopNavBar from '../../molecules/TopNavBar/TopNavBar';
+import FeedCard from '../../organisms/FeedCard/FeedCard';
 import ProfileCategoryPostCard from '../../organisms/ProfileCategoryPostCard/ProfileCategoryPostCard';
 import ProfileInfoCard from '../../organisms/ProfileInfoCard/ProfileInfoCard';
 import TabMenu from '../../organisms/TabMenu/TabMenu';
-import MyProfileTemplateWrapper from './styled';
+import MyProfileTemplateWrapper, {
+  MyProfileAlbumFeedWrapper,
+  MyProfileFeedShowSelectWrapper,
+  MyProfileListFeedWrapper,
+} from './styled';
+import Img from '../../atoms/Img/Img';
+import postListIcon from '../../../assets/images/icon-post-list-off.png';
+import postListOnIcon from '../../../assets/images/icon-post-list-on.png';
+import postAlbumIcon from '../../../assets/images/icon-post-album-off.png';
+import postAlbumOnIcon from '../../../assets/images/icon-post-album-on.png';
 
 const MyProfileTemplate = ({
   profileData,
-  myPostData,
+  postData,
   selectCategoryData,
   onChangeSelectBoxHandler,
+  onClickShowTypeChange,
+  PostShowType,
 }) => {
   return (
     <>
@@ -25,9 +38,50 @@ const MyProfileTemplate = ({
             onChange={onChangeSelectBoxHandler}
             selectCategoryData={selectCategoryData}
           />
-          {/* {myPostData.post.map((item) => (
-            <ProfileFeedCard data={item} key={item.id} />
-          ))} */}
+          <MyProfileFeedShowSelectWrapper>
+            <button
+              type='button'
+              onClick={() => {
+                onClickShowTypeChange('list');
+              }}
+            >
+              <Img
+                src={PostShowType === 'list' ? postListOnIcon : postListIcon}
+              />
+            </button>
+            <button
+              type='button'
+              onClick={() => {
+                onClickShowTypeChange('album');
+              }}
+            >
+              <Img
+                src={PostShowType === 'album' ? postAlbumOnIcon : postAlbumIcon}
+              />
+            </button>
+          </MyProfileFeedShowSelectWrapper>
+          {PostShowType === 'list' && (
+            <MyProfileListFeedWrapper>
+              {postData.map((item) => (
+                <FeedCard key={item.id} data={item} />
+              ))}
+            </MyProfileListFeedWrapper>
+          )}
+          {PostShowType === 'album' && (
+            <MyProfileAlbumFeedWrapper>
+              {postData.map((item) => (
+                <li key={item.id}>
+                  <Link to={`/feeddetail/${item.id}`}>
+                    {item.image.split(', ').length > 1 ? (
+                      <Img src={item.image.split(', ')[0]} />
+                    ) : (
+                      <Img src={item.image} />
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </MyProfileAlbumFeedWrapper>
+          )}
         </MyProfileTemplateWrapper>
         <TabMenu />
       </MainWrapper>
