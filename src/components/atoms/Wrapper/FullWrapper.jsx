49f90688ac5d434d7c6bdc09c600c0439dcb693b
jejeday <jhjae1@naver.com>
2022-12-29 13:37:33 +0900

    Feat : 스플래쉬 전체 틀 Full Wrapper 컴포넌트 생성

---
+++ src/components/atoms/Wrapper/FullWrapper.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MainWrapper = styled.main`
+  max-width: 600px;
+  height: 100vh;
+  margin: 0 auto;
+  position: relative;
+`;
+
+export default MainWrapper;
