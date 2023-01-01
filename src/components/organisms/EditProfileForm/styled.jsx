338083387ef37ae86ef31c38d3321b445445bcd8
Ycow <skypnal12@gmail.com>
2023-01-02 01:38:51 +0900

    Feat : 폼 컴포넌트 에러메세지 스타일링

--- src/components/organisms/EditProfileForm/styled.jsx
+++ src/components/organisms/EditProfileForm/styled.jsx
@@ -1,8 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const EditProfileFormWrapper = styled.form`
+  margin-top: 30px;
   div {
     margin-bottom: 16px;
   }
+
+  ${({ theme }) => {
+    return css`
+      p {
+        color: ${theme.colors.colorWarning};
+        font-size: ${theme.fontSizes.smallMid};
+        margin-bottom: 8px;
+      }
+    `;
+  }}
 `;
 export default EditProfileFormWrapper;
