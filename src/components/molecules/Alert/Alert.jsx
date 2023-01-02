b35d1f5ae081d9038e0bad6f61c59822c4e18331
Ycow <skypnal12@gmail.com>
2023-01-02 16:50:42 +0900

    Feat : Alert 컴포넌트 Link제거하고 useNavigate 적용

--- src/components/molecules/Alert/Alert.jsx
+++ src/components/molecules/Alert/Alert.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { OpacityBg } from '../Modal/styled';
 import AlertWrapper from './styled';
 
