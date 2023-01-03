09e28f83052ebcedd9a015f20cc94160a67715c9
STRONGSHARK <woghfghf@naver.com>
2023-01-03 20:02:11 +0900

    Feat : 카카오맵 화면구현

--- src/components/organisms/CategoryMapCard/styled.jsx
+++ src/components/organisms/CategoryMapCard/styled.jsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+
+const CategoryMapCardWrapper = styled.div`
+  width: 546px;
+  height: 170px;
+  margin: 0 auto;
+`;
+
+export default CategoryMapCardWrapper;
