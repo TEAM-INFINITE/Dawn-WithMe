07f4bf4fd476fbc48d4d4ed67fc848ef301af9f0
Jeongeum <wjddma1202@naver.com>
2023-01-03 14:57:12 +0900

    Feat : 자유게시판 게시물 업로드 템플릿 기능 구현

--- src/components/template/FeedUploadTeplate/FeedUploadTemplate.jsx
+++ src/components/template/FeedUploadTeplate/FeedUploadTemplate.jsx
@@ -2,12 +2,22 @@ import Img from '../../atoms/Img/Img';
 import Textarea from '../../atoms/Textarea/Textarea';
 import HeaderWrapper from '../../atoms/Wrapper/HeaderWrapper';
 import MainWrapper from '../../atoms/Wrapper/MainWrapper';
-import FeedImgBox from '../../molecules/FeedImgBox/FeedImgBox';
-import FeedTextForm from '../../molecules/FeedTextForm/FeedTextForm';
+import FileUpload from '../../molecules/FileUpload/FileUpload';
 import TopNavButtonBar from '../../molecules/TopNavButtonBar/TopNavButtonBar';
-import { FeedUploadWrapper, FeedTextWrapper } from './styled';
+import {
+  FeedUploadWrapper,
+  FeedTextWrapper,
+  DeleteBtn,
+  ImgWrapper,
+} from './styled';
 
-const FeedUploadTemplate = ({ user }) => {
+const FeedUploadTemplate = ({
+  user,
+  onChangeTextHandler,
+  onChangeImagesUpload,
+  onClickSubmit,
+  imgSrc,
+}) => {
   return (
     <>
       <HeaderWrapper>
@@ -15,8 +25,8 @@ const FeedUploadTemplate = ({ user }) => {
           size='ms'
           width='90px'
           form='feedForm'
-          disabled='disabled'
           type='submit'
+          onClick={onClickSubmit}
         >
           업로드
         </TopNavButtonBar>
@@ -25,8 +35,21 @@ const FeedUploadTemplate = ({ user }) => {
         <FeedUploadWrapper>
           <Img src={user.image} alt='프로필 사진' width='42px' height='42px' />
           <FeedTextWrapper>
-            <Textarea placeholder='게시글 입력하기...' />
-            <FeedImgBox />
+            <Textarea
+              placeholder='게시글 입력하기...'
+              onChange={onChangeTextHandler}
+            />
+            <ImgWrapper>
+              <ul>
+                {imgSrc.map((image) => (
+                  <li key={image.id}>
+                    <Img src={image.src} className='priview-img' />
+                    <DeleteBtn />
+                  </li>
+                ))}
+              </ul>
+            </ImgWrapper>
+            <FileUpload onChangeImagesUpload={onChangeImagesUpload} />
           </FeedTextWrapper>
         </FeedUploadWrapper>
       </MainWrapper>
