dc20daba64eca4cb5174430aa2ba909e22e7167b
Ycow <skypnal12@gmail.com>
2023-01-04 09:18:43 +0900

    Feat : 카테고리 페이지 모달 로직 추가

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,11 +1,42 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { isAlertAtom, isModalAtom, modalAtom } from '../../recoil/atom';
 import CategoryTemplate from '../../components/template/CategoryTemplate/CategoryTemplate';
 
 const CategoryPage = () => {
   const location = useLocation();
-  const postListData = location.state.data;
+  const navigate = useNavigate();
+  const [isModal, setIsModal] = useRecoilState(isModalAtom);
+  const [isAlert, setIsAlert] = useRecoilState(isAlertAtom);
+  const [modalValue, setModalValue] = useRecoilState(modalAtom);
 
-  return <CategoryTemplate postListData={postListData} />;
+  const postListData = location.state.data;
+  const textArray = [
+    { id: 1, text: '삭제' },
+    { id: 2, text: '수정' },
+  ];
+
+  const onClickMoreHandler = (id) => {
+    setIsModal(true);
+    setModalValue(id);
+  };
+
+  const onClickModalListHandler = (text) => {
+    if (text === '수정') navigate(`/category/post/${modalValue}`);
+    else if (text === '삭제') setIsAlert(true);
+  };
+
+  console.log(modalValue);
+  return (
+    <CategoryTemplate
+      postListData={postListData}
+      isModal={isModal}
+      setIsModal={setIsModal}
+      onClickMoreHandler={onClickMoreHandler}
+      onClickModalListHandler={onClickModalListHandler}
+      textArray={textArray}
+    />
+  );
 };
 
 export default CategoryPage;
