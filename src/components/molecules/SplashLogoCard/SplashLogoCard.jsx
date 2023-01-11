ba7272f302bc534a5fab3237b67bd532b2709f31
jejeday <jhjae1@naver.com>
2023-01-11 12:11:06 +0900

    Refactor : 스플래쉬 부엉이와 로고 이미지 반복코드를 하나의 컴포넌트로 생성

---
+++ src/components/molecules/SplashLogoCard/SplashLogoCard.jsx
@@ -0,0 +1,17 @@
+import SplashLogoCardWrapper from './styled';
+import Img from '../../atoms/Img/Img';
+import colorFullLogo from '../../../assets/images/color-full-logo.png';
+import titleLogo from '../../../assets/images/title-logo.png';
+import Star from '../../atoms/Star/Star';
+
+const SplashLogoCard = () => {
+  return (
+    <SplashLogoCardWrapper>
+      <Img src={colorFullLogo} width='90%' alt='부엉이 캐릭터' />
+      <Img src={titleLogo} width='70%' alt='던윗미 로고' />
+      <Star className='moon' />
+    </SplashLogoCardWrapper>
+  );
+};
+
+export default SplashLogoCard;
