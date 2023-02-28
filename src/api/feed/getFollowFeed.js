52cce86e856ad2d6ab7dcc391746223c22598e4c
Jeongeum <wjddma1202@naver.com>
2023-02-28 10:12:50 +0900

    Feat : 자유게시판 게시물 목록 무한스크롤 구현

--- src/api/feed/getFollowFeed.js
+++ src/api/feed/getFollowFeed.js
@@ -1,9 +1,16 @@
 import { accessInstance } from '../axios-api';
 
-const getFollowFeed = async () => {
-  const response = await accessInstance.get(`/post/feed/?limit=1000`);
+const getFollowFeed = async (pageParam) => {
+  const response = await accessInstance.get(
+    `/post/feed/?limit=10&skip=${pageParam}`,
+  );
+  const { posts } = response.data;
 
-  return response.data;
+  return {
+    data: posts,
+    nextPage: pageParam,
+    isLast: posts.length % 10 !== 0,
+  };
 };
 
 export default getFollowFeed;
