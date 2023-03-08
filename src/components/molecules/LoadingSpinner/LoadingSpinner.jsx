834a94e37b2c2f62f3e460630311aabdd2c7133e
Jeongeum <wjddma1202@naver.com>
2023-03-09 03:48:14 +0900

    Feat : 다크모드 라이트모드에 따른 로딩스피너 변경

--- src/components/molecules/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/molecules/LoadingSpinner/LoadingSpinner.jsx
@@ -1,11 +1,19 @@
+import { useRecoilState } from 'recoil';
 import Img from '../../atoms/Img/Img';
 import LoadingIcon from '../../../assets/images/loading-spinners.gif';
+import LightLoadingIcon from '../../../assets/images/loading-spinners-light.gif';
 import LoadingSpinnerWrapper from './styled';
+import themeState from '../../../recoil/theme';
 
 const LoadingSpinner = () => {
+  const [themeMode, setThemeMode] = useRecoilState(themeState);
   return (
     <LoadingSpinnerWrapper>
-      <Img src={LoadingIcon} />
+      {themeMode === 'light' ? (
+        <Img src={LightLoadingIcon} />
+      ) : (
+        <Img src={LoadingIcon} />
+      )}
     </LoadingSpinnerWrapper>
   );
 };
